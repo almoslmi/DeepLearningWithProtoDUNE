b27d7f460dd8c5d1326a157ead2747722dd3ee39
Arbin Timilsina <arbin.timilsina@gmail.com>
2018-10-23 15:14:46 -0400

    Update

--- from_dune_tpc/get_csv_file.sh
+++ from_dune_tpc/get_csv_file.sh
@@ -1,3 +1,7 @@
-cd ../../input_files
-rm -rf *.csv
-scp arbint@dunegpvm03.fnal.gov:/nashome/a/arbint/DuneApp/DL-Work/WorkingArea/RootToCSV/*.csv .
+cd ../input_files
+rm -rf training/*.csv
+scp arbint@dunegpvm03.fnal.gov:/nashome/a/arbint/DuneData/RootFiles/DL-Work/RootToCSV/Training/*_w.csv training/
+rm -rf testing/*.csv
+scp arbint@dunegpvm03.fnal.gov:/nashome/a/arbint/DuneData/RootFiles/DL-Work/RootToCSV/Testing/*_w.csv testing/
+rm -rf validation/*.csv
+scp arbint@dunegpvm03.fnal.gov:/nashome/a/arbint/DuneData/RootFiles/DL-Work/RootToCSV/Validation/*_w.csv validation/
