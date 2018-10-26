be34d63e0734285bee693bf8627bd4ca10994ba9
Arbin Timilsina <arbin.timilsina@gmail.com>
2018-10-26 11:59:37 -0400

    Update

--- plotting_tools.py
+++ plotting_tools.py
@@ -1,5 +1,5 @@
 import matplotlib
-matplotlib.use('TkAgg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import numpy as np
@@ -22,10 +22,13 @@ def plot_weights_median(weights, ranges, class_names, plot_name):
         ax.text(median + median/10, max_ - max_/10, 'Median: {:.2f}'.format(median),
                 fontsize=12, fontweight=1000, color='k')
 
-    plt.show()
     fig.savefig(plot_name, bbox_inches='tight')
+    plt.close(fig)
 
 def get_feature_image(feature_image, fig, ax, title):
+    """
+    Setup for feature image.
+    """
     c = ax.imshow(feature_image, cmap='winter_r',interpolation='none', origin='lower',
                   norm=LogNorm(vmin=1.0, vmax=abs(feature_image).max()))
     fig.colorbar(c, ax=ax)
@@ -33,15 +36,18 @@ def get_feature_image(feature_image, fig, ax, title):
     ax.set_ylabel("TDC", fontsize=1, fontname='Georgia',fontweight='bold')
     ax.set_title(title, fontsize=20,fontname='Georgia',fontweight='bold')
 
-def get_label_image(label_image, fig, ax, title):
+def get_label_image(label_image, class_names, fig, ax, title):
+    """
+    Setup for label image.
+    """
     minimum = np.min(label_image)
     maximum = np.max(label_image)
     cmap = plt.get_cmap('gist_heat_r', maximum-minimum+1)
     c= ax.imshow(label_image,cmap=cmap,interpolation='none', origin='lower',
                    vmin=minimum, vmax=maximum)
     cb = fig.colorbar(c, ax=ax)
-    cb.set_ticks(categories_value)
-    cb.set_ticklabels(categories_name)
+    cb.set_ticks([x for x in range(len(class_names))])
+    cb.set_ticklabels(class_names)
 
     ax.set_xlabel("Global wire no.", fontsize=15, fontname='Georgia',fontweight='bold')
     ax.set_ylabel("TDC", fontsize=15, fontname='Georgia',fontweight='bold')
@@ -49,37 +55,46 @@ def get_label_image(label_image, fig, ax, title):
 
 def plot_feature_label(feature_image, label_image,
                        feature_title, label_tile,
-                       plot_name):
+                       class_names, plot_name):
+    """
+    Plot feature and label side by side.
+    """
     fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(20,7), facecolor='w')
 
     get_feature_image(feature_image, fig, ax0, feature_title)
-    get_label_image(label_image, fig, ax1, label_tile)
+    get_label_image(label_image, class_names, fig, ax1, label_tile)
 
-    plt.show()
     fig.savefig(plot_name, bbox_inches='tight')
+    plt.close(fig)
 
-def plot_categories(feature_image, label_image, plot_name):
-    fig, axes = plt.subplots(1, len(categories_value), figsize=(18,12), facecolor='w')
-    for index, value in enumerate(categories_value):
+def plot_categories(feature_image, label_image, class_names, plot_name):
+    """
+    Plot categories side by side.
+    """
+    fig, axes = plt.subplots(1, len(class_names), figsize=(25,7), facecolor='w')
+    for index in range(len(class_names)):
         ax = axes[index]
-        mask = (label_image == value)
+        mask = (label_image == index)
         c = ax.imshow(feature_image*mask, cmap='winter_r',interpolation='none', origin='lower',
                   norm=LogNorm(vmin=1.0, vmax=np.max(feature_image)))
         ax.set_xlabel("Global wire no.", fontsize=15, fontname='Georgia',fontweight='bold')
         ax.set_ylabel("TDC", fontsize=1, fontname='Georgia',fontweight='bold')
-        ax.set_title(categories_name[index], fontsize=20,fontname='Georgia',fontweight='bold')
+        ax.set_title(class_names[index], fontsize=20,fontname='Georgia',fontweight='bold')
 
-    plt.show()
     fig.savefig(plot_name, bbox_inches='tight')
+    plt.close(fig)
 
 def plot_feature_label_prediction(feature_image, label_image, prediction_image,
                                   feature_title, label_title, prediction_title,
-                                  plot_name):
+                                  class_names, plot_name):
+    """
+    Plot feature, label, and prediction side by side.
+    """
     fig, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=(20,5), facecolor='w')
 
     get_feature_image(feature_image, fig, ax0, feature_title)
-    get_label_image(label_image, fig, ax1, label_title)
-    get_label_image(prediction_image, fig, ax2, prediction_title)
+    get_label_image(label_image, class_names, fig, ax1, label_title)
+    get_label_image(prediction_image, class_names, fig, ax2, prediction_title)
 
-    plt.show()
     fig.savefig(plot_name, bbox_inches='tight')
+    plt.close(fig)
