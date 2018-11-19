d90b4e41fad6c23cc83414f311aa53bbd8454c92
Arbin Timilsina <arbin.timilsina@gmail.com>
2018-11-19 10:13:02 -0500

    Optimizer changed to SGD(lr=1e-3, decay=1e-3, momentum=0.9, nesterov=True)

--- train_model.py
+++ train_model.py
@@ -109,7 +109,7 @@ def main():
                            dropout=0.05,
                            batchnorm=True)
 
-    model.compile(optimizer=SGD(lr=1e-5, decay=1e-3, momentum=0.9, nesterov=True),
+    model.compile(optimizer=SGD(lr=1e-3, decay=1e-3, momentum=0.9, nesterov=True),
                   loss=focal_loss(),
                   metrics=['accuracy'])
 
